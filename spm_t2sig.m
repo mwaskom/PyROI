d680b79d103c9429bd6da66a86872f69125666ec
Michael Waskom <mwaskom@mit.edu>
2010-05-25 16:47:15 -0400

    Output is now "spmSig_###.nii'

--- spm_t2sig.m
+++ spm_t2sig.m
@@ -1,57 +1,59 @@
 function spm_t2sig(concell)
-% spm_t2p(contrast cell)
-% Function to convert voxels in spmT images from
-% t-statistics to -log10(p) values for use in FsFast
-% ROI analysis prgrams. Argument can be the name of
+% Convert SPM T maps to -log10(P) maps
 % 
+% Usage: spm_t2sig(concell)  
+% 
+% Inputs:
+%   concell : cell array of paths to SPM contrast directories (can also be a string)
 %
-% Written by Michael Waskom -- mwaskom@mit.edu
-% Last updated March 15, 2010
+% This function will convert all spmT images in an arbitrary number of contrast 
+% directories to -log10(P) images.  It expects to read .img files, and writes to .nii.
+%  
+% Note that this function requires SPM to be on your path, and that it was developed 
+% with SPM 8. It also uses the tTest function provided in the Freesurfer distribution.  
+% ____________________________________________________________________________________
+% $ Written by Michael Waskom -- mwaskom@mit.edu $
 
-if isa(concell,'char')
-   concell = {concell};
-end
+if nargin==0, fprintf('\n   spm_t2sig(contrast directories)\n\n'); return; end
+if isa(concell,'char'), concell = {concell}; end
 
-for iCon = 1:length(concell)
-    convert_tmaps(concell{iCon});
-end
+% Iterate through the contrast directories and convert to sig
+for iCon = 1:length(concell), convert_tmaps(concell{iCon}); end
 
-end
 
 function convert_tmaps(contrastdir)
-% Get the list of spmT images
+% Subroutine for actually performing the conversion from T map to sig map.
+% This function converts all spmT files in a given contrast directory
+
+% Get the list of spmT images in this contrast dir
 fullimginfo = dir([contrastdir '/spmT*.img']);
 for i=1:length(fullimginfo)
     imgs{i} = fullfile(contrastdir,fullimginfo(i).name);
 end
 
-% Get the DOF for this paradigm
+% Get the DOF for this paradigm from the descr string of the first volume
 spmread = spm_vol(imgs{1});
-spmstr = spmread.descrip;
-parseF = strfind(spmstr,'[') + 1;
-parseB = strfind(spmstr,']') - 1;
-dof = str2num(spmstr(parseF:parseB));
+t = regexp(spmread.descrip,'\[([0-9\.]*)\]','tokens')
+dof = str2num(t{1}{1})
 
 % Read in each image, convert to -log10(p), and write
 for iFile = 1:length(imgs)
-    disp(['Reading ' imgs{iFile}]);
+    fprintf('Reading %s\n',imgs{iFile});
     spmThead = spm_vol(imgs{iFile});
     spmTvol = spm_read_vols(spmThead);
-    signvol = spmTvol ./ abs(spmTvol);
     spmPvol = spmTvol;
     spmPvol = tTest(dof,spmPvol);
     spmPvol = -log10(spmPvol);
-    spmPvol = signvol .* spmPvol;
+    spmPvol = sign(spmTvol) .* spmPvol;
     if iFile < 10
-        filename = ['spmP_000' num2str(iFile) '.nii'];
+        filename = ['spmSig_000' num2str(iFile) '.nii'];
     else 
-        filename = ['spmP_00' num2str(iFile) '.nii'];
+        filename = ['spmSig_00' num2str(iFile) '.nii'];
     end
     filename = fullfile(contrastdir,filename);
-    disp(['Writing ' filename]);
+    fprintf('Writing %s\n',filename);
     spmPhead = spmThead;
     spmPhead.fname = filename;
     spm_write_vol(spmPhead,spmPvol);
 end
 
-end
