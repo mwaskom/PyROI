0e44cc2001d12c682867f85f866ba876f68ce946
mwaskom <mwaskom@mit.edu>
2010-07-19 14:44:41 -0400

    Adding test script

---
+++ test.py
@@ -0,0 +1,43 @@
+"""Initial testing framework: merely loop through the atlases/analyses from the
+development config file on debug and run the various methods.  Should suffice for
+now... """
+
+import sys
+import pyroi as roi
+
+native=True
+standard=True
+if "--no-native" in sys.argv: native=False
+if "--no-standard" in sys.argv: standard=False
+
+if native:
+    # Native space analyses
+
+    roi.import_config("/u2/mwaskom/roidevelopment_native.py")
+    res = roi.RoiResult(log=True)
+
+    path = roi.config_file_path()
+    res("Config file: %s" % path)
+
+    for anal in roi.cfg.analysis():
+        for name in roi.cfg.atlases():
+            atlas = roi.init_atlas(name, paradigm=anal["par"], debug=True)
+            res(atlas.group_make_atlas())
+            res(atlas.group_prepare_source_images(anal))
+            res(atlas.group_extract(anal))
+
+if standard:
+    # Standard space analyses
+
+    roi.import_config("/u2/mwaskom/roidevelopment_standard.py")
+    res = roi.RoiResult(log=True)
+
+    path = roi.config_file_path()
+    res("Config file: %s" % path)
+
+    for anal in roi.cfg.analysis():
+        for name in roi.cfg.atlases():
+            atlas = roi.init_atlas(name, paradigm=anal["par"], debug=True)
+            res(atlas.group_make_atlas())
+            res(atlas.group_prepare_source_images(anal))
+            res(atlas.group_extract(anal))
