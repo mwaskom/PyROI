a04df535ac0b0ef834fa26a841d3b57d39e02ac1
mwaskom <mwaskom@mit.edu>
2010-07-19 15:58:20 -0400

    Setup script

--- SetupPyROI.sh
+++ SetupPyROI.sh
@@ -1,5 +1,5 @@
 source /software/python/SetupNiPy26.sh
 export FREESURFER_HOME='/software/Freesurfer/4.5.0/'
 source $FREESURFER_HOME/SetUpFreeSurfer.sh
-export PYTHONPATH=/repos:$PYTHONPATH
-export PATH=/repos:$PATH
+export PYTHONPATH=/repos/pyroi/PyROI-Stable/:$PYTHONPATH
+export PATH=/repos/pyroi/PyROI-Stable/:$PATH
