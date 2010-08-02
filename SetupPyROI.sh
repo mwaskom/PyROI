3faad5c47be5e65cdc23b281d3cdd888b5f370c3
mwaskom <mwaskom@mit.edu>
2010-08-02 00:53:29 -0400

    Setup script changes

--- SetupPyROI.sh
+++ SetupPyROI.sh
@@ -1,4 +1,4 @@
 source /software/python/SetupNiPy26.sh
 fss 4.5.0
 export PYTHONPATH=/repos/pyroi/PyROI-Stable/:$PYTHONPATH
-export PATH=/repos/pyroi/PyROI-Stable/:$PATH
+export PATH=/repos/pyroi/PyROI-Stable/:/repos/pyroi/PyROI-Stable/bin:$PATH
