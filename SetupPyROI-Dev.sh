8a4370b1813ca9d9cf6e1b819c2d58660ffbdbe6
mwaskom <mwaskom@mit.edu>
2010-08-02 22:06:54 -0400

    Modified setup scripts

--- SetupPyROI-Dev.sh
+++ SetupPyROI-Dev.sh
@@ -3,3 +3,4 @@ fss 4.5.0
 export PYTHONPATH=/repos/pyroi/PyROI-Dev:$PYTHONPATH
 export PATH=/repos/pyroi/PyROI-Dev:/repos/pyroi/PyROI-Dev/bin:$PATH
 eval $(/repos/pyroi/setdevprompt.py)
+echo 'The PyROI development environment is now active'
--- SetupPyROI.sh
+++ SetupPyROI.sh
@@ -2,3 +2,4 @@ source /software/python/SetupNiPy26.sh
 fss 4.5.0
 export PYTHONPATH=/repos/pyroi/PyROI-Stable/:$PYTHONPATH
 export PATH=/repos/pyroi/PyROI-Stable/:/repos/pyroi/PyROI-Stable/bin:$PATH
+echo 'The PyROI environment is now active'
